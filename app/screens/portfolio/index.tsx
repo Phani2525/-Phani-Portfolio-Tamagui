7e54f1942296d0248e2840179c0e035feee5eb28
Phani Bhushan <kotharuphani@gmail.com>
2024-01-23 23:52:52 +0530

    added portfolio screen

---
+++ app/screens/portfolio/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {YStack, XStack, Text} from 'ui';
+import { Header } from "app/components/Header";
+
+export const PortfolioScreen = () => {
+    return(
+        <YStack>
+            <Header/><Text>Hello</Text></YStack>
+    )
+}
+
+export default PortfolioScreen;
