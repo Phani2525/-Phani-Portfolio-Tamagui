a37da89a4946515997ead71feb7c6310393c0469
Phani Bhushan <kotharuphani@gmail.com>
2024-01-23 23:53:02 +0530

    added service screen

---
+++ app/screens/service/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {YStack, XStack, Text} from 'ui';
+import { Header } from "app/components/Header";
+
+export const ServiceScreen = () => {
+    return(
+        <YStack>
+            <Header/>
+            <Text>Hello</Text></YStack>
+    )
+}
+
+
