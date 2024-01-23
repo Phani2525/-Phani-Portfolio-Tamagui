051f7c0f3266b4ce19b8609d03463c89debc99c7
Phani Bhushan <kotharuphani@gmail.com>
2024-01-23 10:41:10 +0530

    ✨ feat: Add HomeScreen component with header, scroll view, and content
    This commit adds the `HomeScreen` component to the project. The component includes a header, a scroll view, and some content. It also imports necessary UI components and assets.
    
    Changes:
    - Import React and UI components
    - Define `HomeScreen` component
    - Render a `YStack` container with fullscreen layout
    - Include a `Header` component
    - Add a `ScrollView` component
    - Inside the scroll view, create an `XStack` container with `justifyContent` set to "space-between" and `padding` of 20 pixels
    - Within the `XStack`, add a `YStack` container with `marginTop` of 20 pixels and `ai` (align-items) set to "center"
    - Inside the `YStack`, include multiple `H3` and `Text` components to display information about the developer
    - Add an "About Me" button wrapped in a `Button` component
    - Add a `SimpleImage` component displaying an image
    - Export the `HomeScreen` component as the default export

--- app/screens/home/index.tsx
+++ app/screens/home/index.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import { YStack, Text } from 'ui';
+// HomeScreen.js
+import React, { useState } from 'react';
+import { YStack, XStack, Text, SimpleImage, Button, ScrollView, H3, H4, H2 } from 'ui'; // Assuming ScrollView is part of your UI library
+import { Header } from 'app/components/Header';
 
-function HomeScreen() {
+const HomeScreen = () => {
+  const icon = require('app/assets/icon.png');
+
   return (
     <YStack fullscreen>
-      <Text>Home Screen</Text>
+      <Header />
+      <ScrollView>
+        <XStack justifyContent="space-between" padding={20}>
+          <YStack marginTop="$20" ai="center" space="$6">
+            <H3 fontSize="$10" fontWeight="700" fontStyle="italic">
+              I'm
+            </H3>
+            <H3 fontSize="$12" fontStyle="italic">
+              {' '}
+              K.S.R PHANI BHUSHAN
+            </H3>
+            <Text fontSize={18} fontStyle="italic">
+              Growing React Native Developer | Expanding Skills in JavaScript and TypeScript for both Mobile and Web
+              Applications.
+            </Text>
+            <Button>
+              <Text fontStyle="italic">About Me</Text>
+            </Button>
+          </YStack>{' '}
+          <SimpleImage src={icon} width={900} height={800} />
+        </XStack>
+      </ScrollView>
     </YStack>
   );
-}
+};
 
 export default HomeScreen;
