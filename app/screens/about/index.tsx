485fe759c06ef322a01e3d2504337a2773dce63e
Phani Bhushan <kotharuphani@gmail.com>
2024-01-23 11:53:54 +0530

    ✨ feat: Add SimpleImage component to AboutScreen
    The commit adds the SimpleImage component to the AboutScreen in the app/screens/about/index.tsx file. It also imports the necessary dependencies and uses the SimpleImage component to display an icon image on the screen.

--- app/screens/about/index.tsx
+++ app/screens/about/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { YStack, XStack, Button, Text } from 'ui';
+import { YStack, XStack, Button, Text, SimpleImage } from 'ui';
 import { Header } from 'app/components/Header';
 
 export const AboutScreen = () => {
+  const icon = require('app/assets/icon.png');
   return (
     <YStack fullscreen>
       <Header />
       <Text>hello</Text>
+      <SimpleImage src={icon} width={30} height={40} />
     </YStack>
   );
 };
