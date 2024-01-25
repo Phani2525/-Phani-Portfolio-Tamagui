c80bdd84a0422093b1c000617ad3c915a4f2f36d
Phani Bhushan <kotharuphani@gmail.com>
2024-01-25 10:46:38 +0530

    ♻️ refactor: remove unused imports and add image zoom animation
    In this commit, I refactored the `HomeScreen` component in `index.tsx`. I removed the unused import of the `useState` hook from React and updated the imports from the UI library. Specifically, I removed the import of `H4`, `H2`, and `react-reveal`'s `Zoom` component, as they are not used in the code. Additionally, I added a zoom animation to the `<YStack>` containing the profile information and the `<SimpleImage>` component. This animation enhances the user experience by adding a visual effect when rendering the content.

--- app/screens/home/index.tsx
+++ app/screens/home/index.tsx
@@ -1,7 +1,8 @@
 // HomeScreen.js
-import React, { useState } from 'react';
-import { YStack, XStack, Text, SimpleImage, Button, ScrollView, H3, H4, H2 } from 'ui'; // Assuming ScrollView is part of your UI library
+import React from 'react';
+import { YStack, XStack, Text, SimpleImage, Button, ScrollView, H3 } from 'ui'; // Assuming ScrollView is part of your UI library
 import { Header } from 'app/components/Header';
+import { Zoom } from 'react-reveal';
 
 const HomeScreen = () => {
   const icon = require('app/assets/icon.png');
@@ -11,23 +12,27 @@ const HomeScreen = () => {
       <Header />
       <ScrollView>
         <XStack justifyContent="space-between" padding={20}>
-          <YStack marginTop="$20" ai="center" space="$6">
-            <H3 fontSize="$10" fontWeight="700" fontStyle="italic">
-              I'm
-            </H3>
-            <H3 fontSize="$12" fontStyle="italic">
-              {' '}
-              K.S.R PHANI BHUSHAN
-            </H3>
-            <Text fontSize={18} fontStyle="italic">
-              Growing React Native Developer | Expanding Skills in JavaScript and TypeScript for both Mobile and Web
-              Applications.
-            </Text>
-            <Button>
-              <Text fontStyle="italic">About Me</Text>
-            </Button>
-          </YStack>{' '}
-          <SimpleImage src={icon} width={900} height={800} />
+          <Zoom duration={800} delay={200}>
+            <YStack marginTop="$20" ai="center" space="$6">
+              <H3 fontSize="$10" fontWeight="700" fontStyle="italic">
+                I'm
+              </H3>
+              <H3 fontSize="$12" fontStyle="italic">
+                {' '}
+                K.S.R PHANI BHUSHAN
+              </H3>
+              <Text fontSize={18} fontStyle="italic">
+                Growing React Native Developer | Expanding Skills in JavaScript and TypeScript for both Mobile and Web
+                Applications.
+              </Text>
+              <Button>
+                <Text fontStyle="italic">About Me</Text>
+              </Button>
+            </YStack>
+          </Zoom>
+          <Zoom duration={800} delay={400}>
+            <SimpleImage src={icon} width={900} height={800} />
+          </Zoom>
         </XStack>
       </ScrollView>
     </YStack>
