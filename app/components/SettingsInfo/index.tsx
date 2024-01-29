3932276fe46924d07874ae453ff64f101086ae14
Phani Bhushan <kotharuphani@gmail.com>
2024-01-29 11:50:57 +0530

    ✨ feat: Update About Me section in SettingsInfo component
    The About Me section in the SettingsInfo component has been updated with a detailed description of my skills and experience. It includes information about my proficiency in web and mobile application development using HTML, CSS, React.Js, React-Native, TypeScript, JavaScript, and other front-end technologies. Additionally, it mentions my basic knowledge of Next.Js and Nest.Js for full-stack development and my expertise in DevOps practices such as writing YAML codes, creating Helm Charts, and managing EC2 instances with AWS. The update also highlights my experience in working with Kubernetes, managing clusters, Docker containers, Pods, and creating/managing namespaces. Furthermore, it mentions my familiarity with custom-Debian operating systems, Linux environments, and various DevOps tools like VirtualBox, Boxes, Rancher, OpenLens, K9s, and DigitalOcean. Finally, I express my commitment to delivering high-quality solutions on time, fostering collaboration, and staying updated with industry trends. The About Me section concludes by stating my passion for freelancing, offering services in React Native, and providing expertise in creating static and dynamic websites. Additionally, it mentions my interest in Kubernetes and DevOps services for efficient deployment and management of applications in cloud environments. In conclusion, I express my excitement about collaborating on innovative projects and contributing to the success of future endeavors.

--- app/components/SettingsInfo/index.tsx
+++ app/components/SettingsInfo/index.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import { YStack, H2, XStack, Button, Text, Input } from 'ui';
-import { Paperclip } from '@tamagui/lucide-icons';
+import { YStack, H2, Button, Text } from 'ui';
+import { LinkStack } from 'app/components/LinkStack';
 
 export const SettingsInfo = () => {
   return (
     <YStack space p="$3">
-      <YStack jc="center" p="$3">
-        <H2>Server Settings</H2>
-      </YStack>
-      <Text p="$3">This is where you can change settings related to your server.</Text>
-      <YStack space p="$3" ai="flex-start" jc="flex-start">
-        <H2>Import</H2>
-        <XStack space ai="center">
-          <Paperclip />
-          <Input placeholder="Import export.json" />
-        </XStack>
-        <Button>IMPORT</Button>
-        <H2>Export</H2>
-        <Button>EXPORT</Button>
-      </YStack>
+      <H2>About Me</H2>
+      <Text fontSize={24} fontStyle="italic">
+        Experienced Software Engineer with a strong foundation in Web and Mobile Application Development. Proficient in
+        HTML, CSS, React.Js, React-Native, TypeScript, JavaScript, and a range of Front-End Technologies.
+      </Text>
+
+      <Text fontSize={24} fontStyle="italic">
+        Basic knowledge of Next.Js and Nest.Js for Full-Stack Development. Possesses a moderated level of DevOps skills,
+        including writing YAML codes, creating Helm Charts, and maintaining EC2 instances with AWS. Skilled in
+        Kubernetes, capable of managing Clusters, Docker containers, and Pods, as well as creating and managing
+        Namespaces.
+      </Text>
+
+      <Text fontSize={24} fontStyle="italic">
+        Experienced in Custom-Debian Operating Systems and Linux environments, leveraging tools like VirtualBox, Boxes,
+        Rancher, OpenLens, K9s, and DigitalOcean for efficient DevOps processes. Committed to delivering high-quality
+        solutions on time, fostering collaboration, and staying updated with industry trends.
+      </Text>
+
+      <Text marginTop="$12" fontSize={28} fontStyle="italic">
+        As a passionate individual, I am also actively engaged in freelancing, offering services in React Native for
+        mobile app development, as well as providing expertise in creating static and dynamic websites. My interest
+        extends to Kubernetes and DevOps services, ensuring efficient deployment and management of applications in cloud
+        environments. I am excited about the opportunity to collaborate on innovative projects and contribute to the
+        success of your endeavors.
+      </Text>
+
+      <LinkStack href="/contact" hoverStyle={{ backgroundColor: '$backgroundPress' }} paddingVertical="$3">
+        <Button>Contact Now</Button>
+      </LinkStack>
     </YStack>
   );
 };
