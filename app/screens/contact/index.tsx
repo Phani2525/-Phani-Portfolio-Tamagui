4e2de28ff2af2a35ab6adeec0d407d974f428cc4
Phani Bhushan <kotharuphani@gmail.com>
2024-01-30 18:26:17 +0530

    ♻️ refactor: add slide animations to contact screen
    Added slide animations to the contact screen in order to enhance user experience. The contact image now slides in from the top, and the text on the right side slides in from the right with a delay of 300 milliseconds. The social media buttons also have slide animations, sliding in from the left with different delays. Lastly, the email button has a slide animation from the left with a delay of 400 milliseconds, and the final text at the bottom slides in from the bottom with a delay of 500 milliseconds.

--- app/screens/contact/index.tsx
+++ app/screens/contact/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { YStack, Text, Button, SimpleImage, XStack, ScrollView } from 'ui';
 import { Header } from 'app/components/Header';
 import { Linkedin, Github, Gitlab, Mail } from '@tamagui/lucide-icons';
+import Slide from 'react-reveal/Slide';
 
 const ContactScreen = () => {
   const openLinkInNewTab = (url) => {
@@ -20,74 +21,77 @@ const ContactScreen = () => {
       <Header />
       <ScrollView>
         <XStack space="$4" alignItems="center" justifyContent="center" width="100%" height="100%">
-          <SimpleImage src={contactImage} width={750} height={800} />
+          <Slide top>
+            <SimpleImage src={contactImage} width={750} height={800} />
+          </Slide>
 
           {/* Text and Buttons on the Right Side */}
           <YStack alignItems="center" space="$3" width="50%" padding="$4">
-            <Text marginBottom="$10" fontSize={44} fontWeight="bold" textAlign="center">
-              Contact Me
-            </Text>
+            <Slide right delay={300}>
+              <Text marginBottom="$10" fontSize={44} fontWeight="bold" textAlign="center">
+                Contact Me
+              </Text>
+            </Slide>
 
             <Text fontSize={30} fontStyle="italic" textAlign="center" maxWidth="700px">
               Feel free to reach out to me for any inquiries, collaboration, or just to say hello. You can connect with
               me through the following channels:
             </Text>
 
-            {/* Social Media Links */}
-            <Button
-              onPress={() => openLinkInNewTab('https://github.com/Phani2525')}
-              width="200px"
-              backgroundColor="$blue"
-            >
-              <Github size={24} color="white" />
-              <Text fontSize="$5" color="$white" marginLeft="$1">
-                GitHub
-              </Text>
-            </Button>
+            <Slide left delay={100}>
+              <Button
+                onPress={() => openLinkInNewTab('https://github.com/Phani2525')}
+                width="200px"
+                backgroundColor="$blue"
+              >
+                <Github size={24} color="white" />
+                <Text fontSize="$5" color="$white" marginLeft="$1">
+                  GitHub
+                </Text>
+              </Button>
+            </Slide>
 
-            <Button
-              onPress={() => openLinkInNewTab('https://gitlab.com/phani25')}
-              width="200px"
-              backgroundColor="$green"
-            >
-              <Gitlab size={24} color="white" />
-              <Text fontSize="$5" color="$white" marginLeft="$1">
-                GitLab
-              </Text>
-            </Button>
+            <Slide left delay={200}>
+              <Button
+                onPress={() => openLinkInNewTab('https://gitlab.com/phani25')}
+                width="200px"
+                backgroundColor="$green"
+              >
+                <Gitlab size={24} color="white" />
+                <Text fontSize="$5" color="$white" marginLeft="$1">
+                  GitLab
+                </Text>
+              </Button>
+            </Slide>
 
-            <Button
-              onPress={() => openLinkInNewTab('https://www.linkedin.com/in/phani-bhushan-131b1327a/')}
-              width="200px"
-              backgroundColor="$blue"
-            >
-              <Linkedin size={24} color="white" />
-              <Text fontSize="$5" color="$white" marginLeft="$1">
-                LinkedIn
-              </Text>
-            </Button>
+            <Slide left delay={300}>
+              <Button
+                onPress={() => openLinkInNewTab('https://www.linkedin.com/in/phani-bhushan-131b1327a/')}
+                width="200px"
+                backgroundColor="$blue"
+              >
+                <Linkedin size={24} color="white" />
+                <Text fontSize="$5" color="$white" marginLeft="$1">
+                  LinkedIn
+                </Text>
+              </Button>
+            </Slide>
 
-            {/* <Button
-              onPress={() => openLinkInNewTab('https://www.discord.com/phanibhushan25')}
-              width="200px"
-              backgroundColor="$purple"
-            >
-              <Text fontSize="$5" color="$white" marginRight="$1">
-                Discord
-              </Text>
-            </Button> */}
+            <Slide left delay={400}>
+              <Button onPress={() => openEmailClient('kotharuphani@gmail.com')} width="200px" backgroundColor="$red">
+                <Mail size={24} color="white" />
+                <Text fontSize="$5" color="$white" marginLeft="$1">
+                  Email
+                </Text>
+              </Button>
+            </Slide>
 
-            <Button onPress={() => openEmailClient('kotharuphani@gmail.com')} width="200px" backgroundColor="$red">
-              <Mail size={24} color="white" />
-              <Text fontSize="$5" color="$white" marginLeft="$1">
-                Email
+            <Slide bottom delay={500}>
+              <Text fontSize="$8" color="$white" marginTop="$2">
+                If you're interested in collaborating, have a project, or just want to say hi, feel free to drop me an
+                email!
               </Text>
-            </Button>
-
-            <Text fontSize="$8" color="$white" marginTop="$2">
-              If you're interested in collaborating, have a project, or just want to say hi, feel free to drop me an
-              email!
-            </Text>
+            </Slide>
           </YStack>
         </XStack>
       </ScrollView>
