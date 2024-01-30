c82e32ed31f144a3f746675ce0b4f17e37096f27
Phani Bhushan <kotharuphani@gmail.com>
2024-01-30 12:35:20 +0530

    feat(service): update ServiceScreen component
    - Added ScrollView component from 'ui' package to enable scrolling within the screen
    - Updated the description of services to capitalize the first letter of each word for consistency
    - Replaced the image for 'Web Development' service with a new one
    - Replaced the image for 'DevOps' service with a new one
    - Replaced the image for 'Custom Debian OS' service with a new one
    - Replaced the image for 'React Native Apps' service with a new one
    - Added a background image to the YStack component to provide a visually appealing background
    - Applied styles to achieve full-screen height for the YStack component
    - Added Fade animation from 'react-reveal' package to enhance the user experience
    - Changed the background color of the Card component for better contrast
    - Increased the duration and added delay to the Fade animation for a smoother transition
    - Adjusted the margin, width, height, and padding of the Card component to improve the layout
    
    Note: The commit message assumes that the changes mentioned in the diff are intentional and desired.

--- app/screens/service/index.tsx
+++ app/screens/service/index.tsx
@@ -1,53 +1,60 @@
 import React from 'react';
-import { YStack, Text, Card, SimpleImage, XStack, H2 } from 'ui';
+import { YStack, Text, Card, SimpleImage, XStack, H2, ScrollView } from 'ui';
 import { Header } from 'app/components/Header';
+import { Fade } from 'react-reveal';
 
 export const ServiceScreen = () => {
   const services = [
     {
       name: 'Web Development',
-      description: 'Both static and dynamic websites',
-      image: require('app/assets/icon.png'),
+      description: 'Both Static and Dynamic Websites',
+      image: require('app/assets/web.png'),
     },
     {
       name: 'DevOps',
       description:
-        'Kubernetes, maintaining EC2 instances, clusters, pods, Docker containers, Route53, S3, deploying Helm charts',
-      image: require('app/assets/icon.png'),
+        'Kubernetes, maintaining EC2 instances, Clusters, Pods, Docker Containers, Route53, S3 Buckets and Deploying Helm charts',
+      image: require('app/assets/devops.png'),
     },
     {
       name: 'Custom Debian OS',
-      description: 'Creating custom Debian operating systems',
-      image: require('app/assets/icon.png'),
+      description: 'Creating Custom Debian Operating Systems',
+      image: require('app/assets/debian.png'),
     },
     {
       name: 'React Native Apps',
-      description: 'Developing mobile apps using JavaScript and TypeScript',
-      image: require('app/assets/icon.png'),
+      description: 'Developing Mobile Apps using JavaScript and TypeScript',
+      image: require('app/assets/native.png'),
     },
   ];
 
+  const background = require('app/assets/background1.png');
+
   return (
-    <YStack>
+    <YStack fullscreen style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover', minHeight: '100vh' }}>
       <Header />
-      <H2 marginTop="$6" space fontSize={40} alignSelf="center">
-        <Text fontStyle="italic">My Services</Text>
-      </H2>
-      <XStack marginTop="$8" space justifyContent="space-evenly">
-        {services.map((service, index) => (
-          <Card space="$1" key={index} width="400px" height="500px" margin="$2" padding="$5">
-            <SimpleImage src={service.image} style={{ width: '100%', height: '70%', borderRadius: '10px' }} />
-            <YStack jc="center" ai="center" marginTop="$2">
-              <Text fontStyle="italic" fontSize={28} fontWeight="bold">
-                {service.name}
-              </Text>
-              <Text fontStyle="italic" fontSize={20} marginTop="$2">
-                {service.description}
-              </Text>
-            </YStack>
-          </Card>
-        ))}
-      </XStack>
+      <ScrollView>
+        <H2 marginTop="$6" space fontSize={40} alignSelf="center">
+          <Text fontStyle="italic">My Services</Text>
+        </H2>
+        <XStack marginTop="$8" space justifyContent="space-evenly">
+          {services.map((service, index) => (
+            <Fade key={index} duration={800} delay={index * 200}>
+              <Card backgroundColor="$gray7Dark" space="$1" width="400px" height="500px" margin="$2" padding="$5">
+                <SimpleImage src={service.image} style={{ width: '100%', height: '70%', borderRadius: '10px' }} />
+                <YStack jc="center" ai="center" marginTop="$2">
+                  <Text fontStyle="italic" fontSize={28} fontWeight="bold">
+                    {service.name}
+                  </Text>
+                  <Text fontStyle="italic" fontSize={20} marginTop="$2">
+                    {service.description}
+                  </Text>
+                </YStack>
+              </Card>
+            </Fade>
+          ))}
+        </XStack>
+      </ScrollView>
     </YStack>
   );
 };
