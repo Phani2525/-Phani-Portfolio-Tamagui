5d0b785cb16c0666507e77fd85308b82b83e394c
Phani Bhushan <kotharuphani@gmail.com>
2024-02-08 22:02:24 +0530

    update tab heading

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }: SolitoAppProps) {
   return (
     <>
       <Head>
-        <title>Tamagui Example App</title>
+        <title>K S R PHANI BHUSHAN</title>
         <meta name="description" content="Tamagui, Solito, Expo & Next.js" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
