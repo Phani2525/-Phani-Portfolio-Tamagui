aead05d7a02bf278a49d2dd530bb5cf7d7e5b145
Phani Bhushan <kotharuphani@gmail.com>
2024-03-04 11:59:16 +0530

    🚑 fix: Update styling in SettingsInfo component
    - Updated text styles within the About Me section of the SettingsInfo component to enhance readability and consistency. Made adjustments to font weight, color, and spacing for a more cohesive look across the component.

--- app/components/SettingsInfo/index.tsx
+++ app/components/SettingsInfo/index.tsx
@@ -17,47 +17,51 @@ export const SettingsInfo = () => {
     >
       <YStack>
         <Zoom>
-          <H2 fontStyle="italic" color="#fff">
-            About Me
-          </H2>
-          <div style={{ marginBottom: '24px' }}>
-            <Text space fontSize={24} fontStyle="italic" color="#ddd">
+          <YStack ai="center" fontStyle="italic">
+            <Text fontWeight="700" color="orangered" fontSize={34} fontStyle="italic">
+              About Me
+            </Text>
+          </YStack>
+          <YStack marginTop="$6" style={{ marginBottom: '24px' }}>
+            <Text fontSize={24} fontStyle="italic" color="#ddd">
               Experienced Software Engineer with a strong foundation in Web and Mobile Application
               Development. Proficient in HTML, CSS, React.Js, React-Native, TypeScript, JavaScript,
               and a range of Front-End Technologies.
             </Text>
-          </div>
+          </YStack>
 
-          <div style={{ marginBottom: '24px' }}>
-            <Text space fontSize={24} fontStyle="italic" color="#ddd">
+          <YStack style={{ marginBottom: '24px' }}>
+            <Text fontSize={24} fontStyle="italic" color="#ddd">
               Basic knowledge of Next.Js and Nest.Js for Full-Stack Development. Possesses a
               moderated level of DevOps skills, including writing YAML codes, creating Helm Charts,
               and maintaining EC2 instances with AWS. Skilled in Kubernetes, capable of managing
               Clusters, Docker containers, and Pods, as well as creating and managing Namespaces.
             </Text>
-          </div>
+          </YStack>
 
-          <div style={{ marginBottom: '24px' }}>
+          <YStack style={{ marginBottom: '24px' }}>
             <Text fontSize={24} fontStyle="italic" color="#ddd">
               Experienced in Custom-Debian Operating Systems and Linux environments, leveraging
               tools like VirtualBox, Boxes, Rancher, OpenLens, K9s, and DigitalOcean for efficient
               DevOps processes. Committed to delivering high-quality solutions on time, fostering
               collaboration, and staying updated with industry trends.
             </Text>
-          </div>
+          </YStack>
 
-          <Text marginBottom={6} fontSize={28} fontStyle="italic" color="#fff">
-            As a passionate individual, I am also actively engaged in freelancing, offering services
-            in React Native for mobile app development, as well as providing expertise in creating
-            static and dynamic websites. My interest extends to Kubernetes and DevOps services,
-            ensuring efficient deployment and management of applications in cloud environments. I am
-            excited about the opportunity to collaborate on innovative projects and contribute to
-            the success of your endeavors.
-          </Text>
+          <YStack>
+            <Text marginBottom={6} fontSize={24} fontStyle="italic" color="#fff">
+              As a passionate individual, I am also actively engaged in freelancing, offering
+              services in React Native for mobile app development, as well as providing expertise in
+              creating static and dynamic websites. My interest extends to Kubernetes and DevOps
+              services, ensuring efficient deployment and management of applications in cloud
+              environments. I am excited about the opportunity to collaborate on innovative projects
+              and contribute to the success of your endeavors.
+            </Text>
+          </YStack>
 
           <LinkStack jc="center" href="/contact">
             <Fade>
-              <Button backgroundColor="goldenrod">
+              <Button marginTop="$6" backgroundColor="slateblue">
                 <Text fontWeight="600">Get in Touch</Text>
               </Button>
             </Fade>
